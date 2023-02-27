b429b68708b34b4a40cf3498de5788bd33686432
wubo <boommax1994@hotmail.com>
2023-02-27 08:47:13 +0800

    modify

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,24 @@ import vue from '@vitejs/plugin-vue'
 import ElementPlus from 'unplugin-element-plus/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue(), ElementPlus()]
+	plugins: [vue(), ElementPlus()],
+	base: './',
+	build: {
+		cssCodeSplit: true,
+		sourcemap: false,
+		rollupOptions: {
+			output: {
+				chunkFileNames: 'js/[name]-[hash].js',
+				entryFileNames: 'js/[name]-[hash].js',
+				assetFileNames: '[ext]/[name]-[hash].[ext]'
+			}
+		}
+	},
+	server: {
+		host: '0.0.0.0',
+		port: 8080,
+		open: true,
+		https: false,
+		proxy: {}
+	}
 })
